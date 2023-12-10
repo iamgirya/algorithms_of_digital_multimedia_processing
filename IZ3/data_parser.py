b97c3cac3bee53ef1e9fbf9dbfaa4ad0ca75ebb7
iamgirya <84345515+iamgirya@users.noreply.github.com>
2023-12-10 17:46:35 +0300

    Repair gpu use

--- IZ3/data_parser.py
+++ IZ3/data_parser.py
@@ -1,5 +1,10 @@
 import gzip
+import os
+import pathlib
 import shutil
+import dicom2nifti
+import xlrd
+import csv
 
 
 def unpack_single_gzip_in_folder(folder_path):
@@ -23,15 +28,63 @@ def unpack_single_gzip_in_folder(folder_path):
         print("Ошибка: Не удалось определить единственный файл .gz в указанной папке.")
 
 
-import dicom2nifti
-
-
 # Folder "CT-0" consist of CT scans having normal lung tissue,
 # no CT-signs of viral pneumonia.
+pack_name = "100_200_studies"
+path = (
+    r"dataset\MosMedData-CT-HEMORRHAGE-type VIII\\"
+    + pack_name
+    + "\\"
+    + pack_name
+    + "\\"
+)
+files = os.listdir(path)
+xlsx_files = [file for file in files if file.endswith(".xlxs")]
+if len(xlsx_files) == 1:
+    xlxs_file = xlsx_files[0]
 
-# for x in os.listdir(r"C:\Users\RedMa\OneDrive\Рабочий стол\data\bad\700_800_studies"):
-#     dicom2nifti.convert_directory(os.path.join(r"C:\Users\RedMa\OneDrive\Рабочий стол\data\bad\700_800_studies", x,
-#                                                    os.listdir(os.path.join(
-#                                                        r"C:\Users\RedMa\OneDrive\Рабочий стол\data\bad\700_800_studies",
-#                                                        x))[0]), os.path.join(r"C:\Users\RedMa\OneDrive\Рабочий стол\data\bad\700_800_studies", x))
-#     unpack_single_gzip_in_folder(os.path.join(r"C:\Users\RedMa\OneDrive\Рабочий стол\data\bad\700_800_studies", x))
+    def csv_from_excel():
+        wb = xlrd.open_workbook(path + xlxs_file)
+        sh = wb.sheet_by_name("Clinical params")
+        your_csv_file = open("labels.csv", "w")
+        wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
+        for rownum in range(sh.nrows):
+            wr.writerow(sh.row_values(rownum))
+        your_csv_file.close()
+
+    csv_from_excel()
+
+    labels = {}
+
+    with open(
+        str(rel_path(path + "labels.csv")), newline="", encoding="utf-8"
+    ) as csvfile:
+        reader = csv.reader(csvfile, delimiter=",", quotechar="'")
+        for row in reader:
+            if row[0][0] == "s":
+                continue
+            labels[row[0]] = row[2] == "normal"
+
+# runs the csv_from_excel function:
+csv_from_excel()
+
+for x in os.listdir(r"C:\Users\RedMa\OneDrive\Рабочий стол\data\bad\700_800_studies"):
+    dicom2nifti.convert_directory(
+        os.path.join(
+            r"C:\Users\RedMa\OneDrive\Рабочий стол\data\bad\700_800_studies",
+            x,
+            os.listdir(
+                os.path.join(
+                    r"C:\Users\RedMa\OneDrive\Рабочий стол\data\bad\700_800_studies", x
+                )
+            )[0],
+        ),
+        os.path.join(
+            r"C:\Users\RedMa\OneDrive\Рабочий стол\data\bad\700_800_studies", x
+        ),
+    )
+    unpack_single_gzip_in_folder(
+        os.path.join(
+            r"C:\Users\RedMa\OneDrive\Рабочий стол\data\bad\700_800_studies", x
+        )
+    )
